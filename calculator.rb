8dd621bdf5a66b20ea870ab2dd0d13b77e7b856d
William Reilly <William.Reilly2010@gmail.com>
2016-01-14 12:07:34 -0500

    refactored calculator

--- calculator.rb
+++ calculator.rb
@@ -6,32 +6,93 @@
 # answer = Kernel.gets()
 # Kernel.puts(answer)
 
-Kernel.puts("Welcome to Calculator!")
-
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp().to_f()
-
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp().to_f()
-
-Kernel.puts("What operation would you like to perform? 1: add 2: subtract 3: multiply 4: divide")
-operator_number = Kernel.gets().chomp().to_i
-
-case operator_number
-when 1
-  operator = "+"
-  result = number1 + number2
-when 2
-  operator = "-"
-  result = number1 - number2
-when 3
-  operator = "*"
-  result = number1 * number2
-when 4
-  operator = "/"
-  result = number1 / number2
-else
-  result = "Invalid input"
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-Kernel.puts("#{number1}#{operator}#{number2} = #{result}")
+def valid_number?(number)
+  number.to_f != 0 || number == '0'
+end
+
+
+prompt "Welcome to Calculator!"
+
+name = ''
+loop do
+  prompt "What's your name?"
+  name = Kernel.gets.chomp
+  if name.empty?
+    prompt "Make sure to use a valid name"
+  else
+    break
+  end
+end
+
+prompt "Hi #{name}. Let's get calculating!"
+
+loop do
+  number1 = ''
+  loop do
+    prompt "What's the first number?"
+    number1 = Kernel.gets().chomp()
+    if valid_number?(number1)
+      break
+    else
+      prompt "Hmm.. that doesn't look like a valid number"
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt "What's the second number?"
+    number2 = Kernel.gets().chomp()
+    if valid_number?(number2)
+      break
+    else
+      prompt "Hmm.. that doesn't look like a valid number"
+    end
+  end
+  number1 = number1.to_f
+  number2 = number2.to_f
+  operator_number = ''
+  loop do
+    operator_prompt = <<-MSG
+What operation would you like to perform?
+1: add
+2: subtract
+3: multiply
+4: divide
+    MSG
+    prompt operator_prompt
+    operator_number = Kernel.gets().chomp()
+    if %w(1 2 3 4).include?(operator_number)
+      break
+    else
+      prompt "Invalid operator"
+    end
+  end
+  result = case operator_number
+          when '1'
+            operator = "+"
+            number1 + number2
+          when '2'
+            operator = "-"
+            number1 - number2
+          when '3'
+            operator = "*"
+            number1 * number2
+          when '4'
+            operator = "/"
+            number1 / number2
+          else
+            "Invalid input"
+          end
+
+  prompt "#{number1}#{operator}#{number2} = #{result}"
+
+  prompt "Do you want to perform another calculation? (Y to calculate again)"
+  answer = Kernel.gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thank you, #{name}, for using this Calculator. Have a nice day!"
