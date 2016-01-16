99aa5a7d2740c7cddaad11301ebd40b6cf61a51a
William Reilly <William.Reilly2010@gmail.com>
2016-01-16 17:07:34 -0500

    updated rock paper scissors to include lizard and spock and best to 5

--- rps.rb
+++ rps.rb
@@ -1,12 +1,21 @@
 # Rock Paper Scissors!
 
-VALID_CHOICES = %w(rock paper scissors)
-WINNING_COMBOS = [%w(rock scissors), %w(paper rock), %w(scissors paper)]
+VALID_CHOICES = %w(r p sc l sp)
+WINNING_COMBOS = { rock: %w(lizard scissors), paper: %w(rock spock),
+                   scissors: %w(paper lizard), lizard: %w(paper spock),
+                   spock: %w(rock scissors) }
+NICKNAMES = { 'r' => :rock, 'p' => :paper, 'sc' => :scissors,
+              'l' => :lizard, 'sp' => :spock }
+GAME_NAME = "Rock, Paper, Scissors, Lizard, Spock"
+
+def win?(player1, player2)
+  WINNING_COMBOS[NICKNAMES[player1]].include? NICKNAMES[player2].to_s
+end
 
 def display_results(player, computer)
   if player == computer
     prompt "Tie!"
-  elsif WINNING_COMBOS.include? [player, computer]
+  elsif win? player, computer
     prompt "You won!"
   else
     prompt "You lose!"
@@ -17,10 +26,40 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-loop do
+def display_score(score)
+  prompt "Player: #{score[0]} v. Computer: #{score[1]}"
+end
+
+def end_of_game?(score)
+  score.include? 5
+end
+
+def display_winner(score)
+  if score[0] > score[1]
+    prompt "You beat the computer! #{score[0]} to #{score[1]}."
+  else
+    prompt "Nice try. The computer beat you #{score[1]} to #{score[0]}."
+  end
+end
+
+def page_break
+  puts '-' * 40
+end
+
+prompt "Welcome to #{GAME_NAME}!"
+score = [0, 0]
+until end_of_game? score
   choice = ''
+
+  display_score score
+
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+
+    NICKNAMES.each do |key, value|
+      prompt "#{key}::#{value}"
+    end
+
     choice = Kernel.gets.chomp.downcase
 
     if VALID_CHOICES.include? choice
@@ -31,14 +70,26 @@ loop do
   end
 
   computer_choice = VALID_CHOICES.sample
+  if win? choice, computer_choice
+    score[0] += 1
+  elsif choice == computer_choice
+    # do nothing
+  else
+    score[1] += 1
+  end
 
-  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+  Kernel.puts("You chose: #{NICKNAMES[choice]};
+    Computer chose: #{NICKNAMES[computer_choice]}")
 
   display_results choice, computer_choice
 
-  prompt 'Do you want to play again?'
-  answer = Kernel.gets.chomp.downcase
-  break unless %w(y yes).include? answer
+  page_break
+
+  # prompt 'Do you want to play again?'
+  # answer = Kernel.gets.chomp.downcase
+  # break unless %w(y yes).include? answer
 end
 
-prompt 'Thank you for playing Rock, Paper, Scissors!'
+display_winner score
+
+prompt "Thank you for playing #{GAME_NAME}!"
